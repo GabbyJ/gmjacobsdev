db9fecb1f9d35c98936139af5b5480e2c952a1f7
Gabrielle Jacobs <GabbyJ424@gmail.com>
2025-01-05 23:03:20 -0600

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,15 @@ function AboutSection(){
         <section className="about" id="about">
         <h2 data-aos="fade-up" data-aos-duration="700">About Me</h2>
         <div className="summary">
-        <h3 className="aboutHeadline" data-aos="fade-up" data-aos-duration="700">A curiosity driven Front End Developer with multimedia chops</h3>
+        <h3 className="aboutHeadline" data-aos="fade-up" data-aos-duration="700">A curiosity-driven Web Producer with a knack for front end development and multimedia</h3>
         <div data-aos="fade-up" data-aos-duration="700">
-          <p>I am passionate about bringing both the technical and visual aspects of digital products to life. I care deeply about the user experience, beautiful pixels, and writing clean accessible code that's readable by humans.</p>
-          <p>I'm happiest when I'm creating, learning, exploring, and thinking about how to make things better and solve problems.</p>
+          <p>I am passionate about creating seamless digital experiences by blending technical expertise with creative problem-solving. As a Web Producer, I specialize in managing and enhancing web content, streamlining workflows, and delivering projects that prioritize usability and accessibility.</p>
+          <p>I care deeply about user experience, beautiful design, and ensuring digital solutions are intuitive and functional. While I’ve honed my skills as a Front-End Developer, my focus has evolved into orchestrating web production processes that bring digital visions to life.</p>
+            <p>I'm happiest when I'm creating, learning, exploring, and thinking about how to make things better and solve problems.</p>
           <p>I'm a born and raised Pittsburgher who now lives in Dallas, TX.
-            I'm a self-taught web developer who loves learning as much as I can. In 
-            my free time, I'm usually playing an instrument, watching movies,
-            or traveling the world. Keep scrolling down to find out more about me!
+            I’m self-taught and love learning as much as I can. In 
+            my free time, you can find me playing an instrument, watching movies, trying a new hobby,
+            or traveling the world. Keep scrolling to find out more about me!
           </p>
         </div>
         </div>
